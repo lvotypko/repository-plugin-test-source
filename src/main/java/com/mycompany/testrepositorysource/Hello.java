859612a91e85e9d02be83220ee9ab818abbf6d96
lvotypko <lvotypko@redhat.com>
2014-07-17 17:05:09 +0200

    Init

---
+++ src/main/java/com/mycompany/testrepositorysource/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testrepositorysource;
+
+/**
+ *
+ * @author lucinka
+ */
+public class Hello {
+    
+    String greeting;
+    
+    public Hello(String greeting){
+        this.greeting=greeting;
+    }
+    
+    public void greetings(){
+        System.out.println(greeting);
+    }
+}
